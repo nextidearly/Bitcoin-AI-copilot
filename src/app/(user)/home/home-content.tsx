0be4c75824fc84fb1dd256d012054b7a1cdb6954
nextidearly <talenteddev0125@gmail.com>
2025-04-22 02:51:37 +0800

    Add me, okx, xverse and leather payment

--- src/app/(user)/home/home-content.tsx
+++ src/app/(user)/home/home-content.tsx
@@ -66,7 +66,6 @@ export function HomeContent() {
     }
 
     if (!user.earlyAccess) {
-      setShowPayment(true);
       return;
     }
 
--- src/components/payment-card.tsx
+++ src/components/payment-card.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
+import {
+  AddressPurpose,
+  BitcoinNetworkType,
+  RpcErrorCode,
+  getAddress,
+  getProviderById,
+  sendBtcTransaction,
+} from 'sats-connect';
 import { toast } from 'sonner';
 
 import { Badge } from './ui/badge';
@@ -13,6 +22,8 @@ const RECEIVE_WALLET_ADDRESS =
 declare global {
   interface Window {
     unisat: any;
+    okxwallet: any;
+    magicEden: any;
   }
 }
 
@@ -23,11 +34,22 @@ export default function PaymentCard({
   user_id: string | undefined;
   setModel: Function;
 }) {
+  const router = useRouter();
   const [isProcessing, setIsProcessing] = useState('');
+
+  const handleError = (error: any) => {
+    if (error?.error?.message) toast.error(error.error.message);
+    else if (error?.messages) toast.error(error.messages[0]);
+    else if (error?.message) toast.error(error.message);
+    else if (error?.msg) toast.error(error.msg);
+    else toast.error(error.toString());
+    setIsProcessing('');
+  };
 
   const handlePayWithUnisat = async () => {
     try {
       if (!user_id) {
+        setIsProcessing('');
         return null;
       }
 
@@ -49,19 +71,214 @@ export default function PaymentCard({
         setIsProcessing('');
         setModel(false);
         toast.success('You have paied successfully!');
+        router.refresh();
       }
     } catch (error: any) {
-      if (error?.message) {
-        toast.error(error?.message);
-      } else if (error?.messages) {
-        toast.error(error?.messages[0]);
-      } else if (error?.msg) {
-        toast.error(error?.msg);
-      } else {
-        toast.error(error.toString());
+      handleError(error);
+    }
+  };
+
+  const handlePayWithXverse = async () => {
+    try {
+      if (!user_id) {
+        setIsProcessing('');
+        return null;
       }
 
-      setIsProcessing('');
+      const xverseProvider = getProviderById('XverseProviders.BitcoinProvider');
+
+      if (!xverseProvider) {
+        toast.error('Xverse wallet is not installed');
+        setIsProcessing('');
+        return null;
+      }
+
+      const response = await xverseProvider.request('sendTransfer', {
+        recipients: [
+          {
+            address: RECEIVE_WALLET_ADDRESS,
+            amount: Number(EAP_PRICE * 10 ** 8),
+          },
+        ],
+      });
+
+      if (response.status === 'success') {
+        await fetch('/api/user', {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        setIsProcessing('');
+        setModel(false);
+        toast.success('You have paied successfully!');
+        router.refresh();
+      } else {
+        if (response.error.code === RpcErrorCode.USER_REJECTION) {
+          toast.error('User rejected request');
+        } else {
+          toast.error('Payment failed');
+        }
+        setIsProcessing('');
+      }
+    } catch (error: any) {
+      handleError(error);
+    }
+  };
+
+  const handlePayWithLeather = async () => {
+    try {
+      if (!user_id) {
+        setIsProcessing('');
+        return null;
+      }
+
+      const LeatherProvider = getProviderById('LeatherProvider');
+
+      if (!LeatherProvider) {
+        toast.error('Leather wallet is not installed');
+        setIsProcessing('');
+        return null;
+      }
+
+      const response = await LeatherProvider.request('sendTransfer', {
+        recipients: [
+          {
+            address: RECEIVE_WALLET_ADDRESS,
+            amount: (EAP_PRICE * 10 ** 8).toString(),
+          },
+        ],
+      });
+
+      if (response.status === 'success') {
+        await fetch('/api/user', {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        setIsProcessing('');
+        setModel(false);
+        toast.success('You have paied successfully!');
+        router.refresh();
+      } else {
+        if (response.error.code === RpcErrorCode.USER_REJECTION) {
+          toast.error('User rejected request');
+        } else {
+          toast.error('Payment failed');
+        }
+        setIsProcessing('');
+      }
+    } catch (error: any) {
+      handleError(error);
+    }
+  };
+
+  const handlePayWithOkx = async () => {
+    try {
+      if (!user_id) {
+        setIsProcessing('');
+        return null;
+      }
+
+      const okxProvider = window?.okxwallet?.bitcoin;
+      await okxProvider.connect();
+
+      if (!okxProvider) {
+        toast.error('Okx wallet is not installed');
+        setIsProcessing('');
+        return null;
+      }
+
+      const txid = await okxProvider.sendBitcoin(
+        RECEIVE_WALLET_ADDRESS,
+        EAP_PRICE * 10 ** 8,
+      );
+
+      if (txid) {
+        await fetch('/api/user', {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        setIsProcessing('');
+        setModel(false);
+        toast.success('You have paied successfully!');
+        router.refresh();
+      } else {
+        toast.error('Payment failed');
+        setIsProcessing('');
+      }
+    } catch (error: any) {
+      handleError(error);
+    }
+  };
+
+  const handlePayWithMagicEden = async () => {
+    try {
+      if (!user_id) {
+        setIsProcessing('');
+        return null;
+      }
+
+      const magicedenProvider = window?.magicEden?.bitcoin;
+
+      if (!magicedenProvider) {
+        toast.error('Magic Eden wallet is not installed');
+        setIsProcessing('');
+        return null;
+      }
+
+      let senderAddress = '';
+
+      await getAddress({
+        getProvider: () => magicedenProvider,
+        payload: {
+          purposes: [AddressPurpose.Payment],
+          message: 'Address for receiving Ordinals and payments',
+          network: {
+            type: BitcoinNetworkType.Mainnet,
+          },
+        },
+        onFinish: (response) => {
+          senderAddress = response.addresses[0].address;
+        },
+        onCancel: () => {
+          toast.error('Request canceled');
+        },
+      });
+
+      await sendBtcTransaction({
+        getProvider: () => magicedenProvider,
+        payload: {
+          network: {
+            type: BitcoinNetworkType.Mainnet,
+          },
+          recipients: [
+            {
+              address: RECEIVE_WALLET_ADDRESS!,
+              amountSats: BigInt(EAP_PRICE * 10 ** 8),
+            },
+          ],
+          senderAddress: senderAddress!,
+        },
+        onFinish: async (response) => {
+          console.log(response);
+          await fetch('/api/user', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+          });
+
+          setIsProcessing('');
+          setModel(false);
+          toast.success('You have paied successfully!');
+          router.refresh();
+        },
+        onCancel: () => {
+          setIsProcessing('');
+          toast.error('Payment failed');
+        },
+      });
+    } catch (error: any) {
+      handleError(error);
     }
   };
 
@@ -84,8 +301,6 @@ export default function PaymentCard({
             <button
               className="flex w-full items-center gap-3 bg-gray-200/50 p-[5px] font-[16px] hover:bg-gray-200/80"
               onClick={() => {
-                console.log('test');
-
                 setIsProcessing('unisat');
                 handlePayWithUnisat();
               }}
@@ -104,7 +319,13 @@ export default function PaymentCard({
                 </span>
               )}
             </button>
-            <button className="flex w-full items-center gap-3 bg-gray-200/50 p-[5px] font-[16px] hover:bg-gray-200/80">
+            <button
+              className="flex w-full items-center gap-3 bg-gray-200/50 p-[5px] font-[16px] hover:bg-gray-200/80"
+              onClick={() => {
+                setIsProcessing('xverse');
+                handlePayWithXverse();
+              }}
+            >
               <Image
                 src={'/wallet/xverse.jpg'}
                 className="h-8 w-8 rounded-lg"
@@ -112,9 +333,20 @@ export default function PaymentCard({
                 width={32}
                 height={32}
               />
-              Xverse
+              <span>Xverse</span>
+              {isProcessing === 'xverse' && (
+                <span className="ml-auto text-xs text-gray-400">
+                  Processing...
+                </span>
+              )}
             </button>
-            <button className="flex w-full items-center gap-3 bg-gray-200/50 p-[5px] font-[16px] hover:bg-gray-200/80">
+            <button
+              className="flex w-full items-center gap-3 bg-gray-200/50 p-[5px] font-[16px] hover:bg-gray-200/80"
+              onClick={() => {
+                setIsProcessing('magiceden');
+                handlePayWithMagicEden();
+              }}
+            >
               <Image
                 src={'/wallet/magiceden.png'}
                 alt="wallet"
@@ -122,9 +354,20 @@ export default function PaymentCard({
                 width={32}
                 height={32}
               />
-              MagicEden
+              <span>MagicEden</span>
+              {isProcessing === 'magiceden' && (
+                <span className="ml-auto text-xs text-gray-400">
+                  Processing...
+                </span>
+              )}
             </button>
-            <button className="flex w-full items-center gap-3 bg-gray-200/50 p-[5px] text-sm font-[16px] hover:bg-gray-200/80">
+            <button
+              className="flex w-full items-center gap-3 bg-gray-200/50 p-[5px] text-sm font-[16px] hover:bg-gray-200/80"
+              onClick={() => {
+                setIsProcessing('okx');
+                handlePayWithOkx();
+              }}
+            >
               <Image
                 src={'/wallet/okx.png'}
                 alt="wallet"
@@ -133,8 +376,19 @@ export default function PaymentCard({
                 height={32}
               />
               Okx
+              {isProcessing === 'okx' && (
+                <span className="ml-auto text-xs text-gray-400">
+                  Processing...
+                </span>
+              )}
             </button>
-            <button className="flex w-full items-center gap-3 bg-gray-200/50 p-[5px] font-[16px] hover:bg-gray-200/80">
+            <button
+              className="flex w-full items-center gap-3 bg-gray-200/50 p-[5px] font-[16px] hover:bg-gray-200/80"
+              onClick={() => {
+                setIsProcessing('leather');
+                handlePayWithLeather();
+              }}
+            >
               <Image
                 src={'/wallet/leather.jpg'}
                 alt="wallet"
@@ -143,6 +397,11 @@ export default function PaymentCard({
                 height={32}
               />
               Leather
+              {isProcessing === 'leather' && (
+                <span className="ml-auto text-xs text-gray-400">
+                  Processing...
+                </span>
+              )}
             </button>
           </div>
 
@@ -153,7 +412,7 @@ export default function PaymentCard({
                 {EAP_PRICE} BTC
               </span>
             </div>
-            <div className="rounded-lg border border-teal-600 bg-teal-500/10 p-3 text-xs sm:text-sm">
+            <div className="rounded-lg border border-teal-600 bg-teal-500/10 p-3 text-xs text-gray-500 sm:text-sm">
               Funds will be allocated to cover expenses such as LLM integration,
               RPC data services, infrastructure maintenance, and other
               operational costs, all aimed at ensuring the platform&apos;s

41da24464986cdb3d9a813b8c977c8c59e989526
nextidearly <talenteddev0125@gmail.com>
2025-06-02 20:30:05 +0800

    feat: add login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <main className="flex-1">
-        {/* <Hero handleLogin={login} /> */}
+        <Hero handleLogin={login} />
       </main>
     </div>
   );
